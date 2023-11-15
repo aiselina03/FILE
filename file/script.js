dd579a9af62bfe6a938e5c51202a13089efbf0b8
Aysel <146956428+aiselina03@users.noreply.github.com>
2023-11-15 17:32:16 +0400

    Add files via upload

---
+++ file/script.js
@@ -0,0 +1,32 @@
+//img ucun
+// const inp=document.getElementById("inp")
+// const img=document.getElementById("img")
+
+// inp.onchange=()=>{
+//     const myFile=inp.files[0]
+//     const reader=new FileReader()
+// console.log(inp.files);
+//     reader.readAsDataURL(myFile)
+
+//     reader.onload=()=>{
+//         img.src=reader.result
+//     }
+
+// }
+
+
+//txt file ucun
+// const inp=document.getElementById("inp")
+// const img=document.querySelector("img")
+// const text=document.querySelector(".text")
+// inp.onchange=()=>{
+//     const myFile=inp.files[0]
+//     const reader=new FileReader()
+// console.log(inp.files);
+//     reader.readAsText(myFile)
+
+//     reader.onload=()=>{
+//         text.textContent=reader.result
+//     }
+
+// }
